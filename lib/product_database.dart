096180e7657c2d62b50ec74039f4afe0795a82b2
MACS-AR <96332901+MACS-AR@users.noreply.github.com>
2025-01-22 15:13:03 +0200

    Create product_database.dart

---
+++ lib/product_database.dart
@@ -0,0 +1,34 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class ProductDatabase {
+  static final ProductDatabase instance = ProductDatabase._init();
+
+  static Database? _database;
+
+  ProductDatabase._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initDB('supermarket_pos.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE products (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        price REAL NOT NULL,
+        barcode TEXT NOT NULL
+      );
+    ''');
+  }
+}
